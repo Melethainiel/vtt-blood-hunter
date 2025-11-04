e90e5a3f55ebb4b3965bed7c6aaa683a81aab723
Melethainiel <melethainiel@example.com>
2025-11-04 09:34:51 +0000

    fix: Change ESLint sourceType from 'script' to 'module'
    The codebase uses ES6 import/export statements which require
    sourceType: 'module' in the ESLint parser configuration.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'script'
+    sourceType: 'module'
   },
   rules: {
     // Erreurs critiques - toujours actives
